10137db0566b1f133754a083d3e54a50be79edc6
lizvidotti91 <liz.vidotti@gmail.com>
2020-07-21 17:10:13 -0300

    inserindo js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const root = window.document.querySelector('#root');
+const title = '<h1>Dificuldade</h1>';
+
+root.innerHTML = title;
+
+/*
+function newElement(tag,content){
+    const title = `<${tag}> ${content} <${tag}>`;
+    root.insertAdjacentHTML('beforeend',title);
+}
+
+newElement('h1','Dificuldade');
+*/
